c970d4fcaeb4fbb2208d7bc5ef61762ddcb75b23
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-03-28 08:41:04 +0900

    add router

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use("/posts", postsRouter);
 app.use("/users", usersRouter);
-app.use("/inquiries", inquiriesRouter)
+app.use("/inquiries", inquiriesRouter);
 
 app.get("/", (req, res) => {
   res.send("PACE API");
