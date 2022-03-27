70b79c91a3717850417db24844a6d19b7322ab8a
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-03-27 16:24:53 +0900

    move update

--- backend/src/posts/controllers.ts
+++ backend/src/posts/controllers.ts
@@ -1,39 +1,6 @@
 import { Request, Response } from "express";
 import sql from "../db";
 
-export const updatePost = async (req: Request, res: Response) => {
-  const title = req.body.title;
-  const body = req.body.body;
-  const pictureProvided = req.body.picture;
-  const id = Number(req.params.id);
-
-  try {
-    if (typeof title !== "string") throw Error("No title provided.");
-    if (typeof body !== "string") throw Error("No body provided");
-    if (isNaN(id)) throw TypeError("Id must be a string");
-
-    const pictureHex =
-      typeof pictureProvided === "string"
-        ? convertB64StringToHex(pictureProvided)
-        : null;
-
-    const [result] = await sql`UPDATE posts SET ${sql({
-      title,
-      body,
-      picture: pictureHex,
-    })}
-    WHERE id = ${id} RETURNING id`;
-
-    res.json({ id: result.id, updated: true });
-  } catch (error) {
-    res.statusCode = 400;
-    res.json({
-      message: error instanceof Error ? error.message : "Invalid request.",
-      updated: false,
-    });
-  }
-};
-
 export const deletePost = async (req: Request, res: Response) => {
   const id = Number(req.params.id);
 
--- backend/src/posts/index.ts
+++ backend/src/posts/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
-import { deletePost, updatePost } from "./controllers";
+import { deletePost } from "./controllers";
 import getPosts from "./controllers/get-posts";
 import newPost from "./controllers/new-post";
+import updatePost from "./controllers/update-post";
 
 const postsRouter = express.Router();
 
