68d67e0e04d02749431dea5e7a2a5c0ab6174b18
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-03-27 15:57:55 +0900

    add picture conversion to base64 for get

--- backend/src/posts/controllers.ts
+++ backend/src/posts/controllers.ts
@@ -1,6 +1,15 @@
 import { Request, Response } from "express";
 import sql from "../db";
 
+interface PostEntry {
+  id: number;
+  title: string;
+  body: string;
+  picture: string;
+  created_at: string;
+  updated_at: string;
+}
+
 export const getPostsByPage = async (req: Request, res: Response) => {
   const pageNo = req.query.page_no ? Number(req.query.page_no) : 0;
   const limit = req.query.limit ? Number(req.query.limit) : 10;
@@ -9,13 +18,21 @@ export const getPostsByPage = async (req: Request, res: Response) => {
     if (isNaN(pageNo)) throw Error("Page No must be a number");
     if (isNaN(limit)) throw Error("Limit must be a number");
 
-    const posts =
-      await sql`SELECT id, title, body, picture, created_at, updated_at
+    const posts = await sql<
+      PostEntry[]
+    >`SELECT id, title, body, picture, created_at, updated_at
         FROM posts
         ORDER BY created_at DESC
         LIMIT ${limit} OFFSET ${pageNo * limit};`;
 
-    res.json(posts);
+    res.json(
+      posts.map(post => {
+        if (post.picture) {
+          post.picture = Buffer.from(post.picture, "hex").toString("base64");
+        }
+        return post;
+      })
+    );
   } catch (error) {
     res.statusCode = 400;
     if (error instanceof Error) {
@@ -29,12 +46,11 @@ export const newPost = async (req: Request, res: Response) => {
   const body = req.body.body;
   const pictureParam = req.body.picture;
 
-
   try {
     const picture =
-    typeof pictureParam === "string"
-      ? Buffer.from(pictureParam, "base64").toString("hex") // must do some security work here
-      : null; // convert base64 image to hex binary
+      typeof pictureParam === "string"
+        ? convertB64StringToHex(pictureParam) // must do some security work here
+        : null; // convert base64 image to hex binary
     if (typeof title !== "string") throw Error("No title provided.");
     if (typeof body !== "string") throw Error("No body provided");
 
@@ -53,8 +69,17 @@ export const newPost = async (req: Request, res: Response) => {
 export const updatePost = async (req: Request, res: Response) => {
   const title = req.body.title;
   const body = req.body.body;
+  const pictureParam = req.body.picture;
 
-  res.json("test");
+  try {
+    if (typeof title !== "string") throw Error("No title provided.");
+    if (typeof body !== "string") throw Error("No body provided");
+    if (pictureParam) {
+    } else {
+    }
+  } catch (error) {
+    res.statusCode = 400;
+  }
 };
 
 export const deletePost = async (req: Request, res: Response) => {
@@ -72,3 +97,6 @@ export const deletePost = async (req: Request, res: Response) => {
     res.statusCode = 400;
   }
 };
+
+const convertB64StringToHex = (b64String: string) =>
+  Buffer.from(b64String, "base64").toString("hex");
