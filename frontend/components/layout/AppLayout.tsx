89f301e2a3b2d4af3dc1739a2ff2af712abbbaf4
Austin Mayer <a.inoue@churadata.okinawa>
2022-03-28 17:00:59 +0900

    format

--- frontend/components/layout/AppLayout.tsx
+++ frontend/components/layout/AppLayout.tsx
@@ -1,8 +1,16 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const AppLayout: React.FC = props => {
-  const links = [{ title: "法人説明", link: "/about" }, { title: "ブログ", link: "/blog"  }, { title: "お問い合わせ", link: "/inquiry"  }];
+  const router = useRouter();
+
+  const links = [
+    { title: "ホーム", link: "/" },
+    { title: "法人説明", link: "/about" },
+    { title: "ブログ", link: "/blog" },
+    { title: "お問い合わせ", link: "/inquiry" },
+  ];
   return (
     <>
       <nav>
