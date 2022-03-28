400c9823c9b5115db92b994357f063ab6cb64745
Austin Mayer <a.inoue@churadata.okinawa>
2022-03-28 16:46:08 +0900

    add basic pages

---
+++ frontend/pages/about/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const About: NextPage = () => {
+  return <div>About us Page</div>;
+};
+
+export default About;
---
+++ frontend/pages/inquiry/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Inquiry: NextPage = () => {
+  return <div>Inquiry Page</div>;
+};
+
+export default Inquiry;
