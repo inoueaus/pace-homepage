bbfd1a8021a621f60bfef33304cafd39018f3c80
Austin Mayer <a.inoue@churadata.okinawa>
2022-03-28 12:46:22 +0900

    change json to camelCase

--- backend/src/inquiries/controllers/get-inquiries.ts
+++ backend/src/inquiries/controllers/get-inquiries.ts
@@ -1,6 +1,16 @@
 import { Request, Response } from "express";
 import sql from "../../db";
 
+interface InquiryDBRecord {
+  inquiry_id: number;
+  body: string;
+  phone: string;
+  email: string;
+  first_name: string;
+  last_name: string;
+  created_at: string;
+}
+
 const getInquiries = async (req: Request, res: Response) => {
   const pageNo = req.query.page ? Number(req.query.page) : 0; // default page 1
   const limit = req.query.limit ? Number(req.query.limit) : 10; // default 10 per page
@@ -9,13 +19,25 @@ const getInquiries = async (req: Request, res: Response) => {
     if (isNaN(pageNo)) throw Error("Page No must be a number");
     if (isNaN(limit)) throw Error("Limit must be a number");
 
-    const result =
-      await sql`SELECT inquiry_id as id, body, email, phone, first_name as firstName, last_name as lastName, created_at as createdAt
+    const result = await sql<
+      InquiryDBRecord[]
+    >`SELECT inquiry_id, body, email, phone, first_name, last_name, created_at
     FROM inquiries
     ORDER BY created_at DESC
     LIMIT ${limit} OFFSET ${limit * pageNo}`;
 
-    res.json(result);
+    res.json(
+      result.map(inquiry => ({
+        // must format to camel case as per JSON standards
+        id: inquiry.inquiry_id,
+        body: inquiry.body,
+        phone: inquiry.phone,
+        email: inquiry.email,
+        firstName: inquiry.first_name,
+        lastName: inquiry.last_name,
+        createdAt: inquiry.created_at,
+      }))
+    );
   } catch (error) {
     res.status(400).json({
       message: error instanceof Error ? error.message : "Invalid request.",
