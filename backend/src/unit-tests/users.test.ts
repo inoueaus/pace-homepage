d370ce3fccf89fb00f0440ca92b9add860b56aed
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-03-29 07:25:40 +0900

    add users test

---
+++ backend/src/unit-tests/users.test.ts
@@ -0,0 +1,15 @@
+import app from "../app";
+import request from "supertest";
+import sql from "../db";
+
+describe("Inquiries Router Tests", () => {
+  test("Get all Inquiries", () => {
+    return request(app)
+      .get("/users")
+      .then(response => {
+        expect(response.statusCode).toBe(200);
+      });
+  });
+
+  afterAll(() => sql.end());
+});
