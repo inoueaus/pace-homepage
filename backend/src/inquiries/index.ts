d72ca7b1b48754563df8920504726f6c52f892cc
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-03-29 09:01:30 +0900

    add auth to inquiries

--- backend/src/inquiries/index.ts
+++ backend/src/inquiries/index.ts
@@ -3,15 +3,16 @@ import getInquiries from "./controllers/get-inquiries";
 import getSingleInquiry from "./controllers/get-single-inquiry";
 import newInquiry from "./controllers/new-inquiry";
 import deleteInquiry from "./controllers/delete-inquiry";
+import requireToken from "../middleware/authentication";
 
 const inquiriesRouter = Router();
 
-inquiriesRouter.get("/", getInquiries);
+inquiriesRouter.get("/", requireToken, getInquiries);
 
-inquiriesRouter.get("/:id", getSingleInquiry);
+inquiriesRouter.get("/:id", requireToken, getSingleInquiry);
 
 inquiriesRouter.post("/new", newInquiry);
 
-inquiriesRouter.delete("/:id", deleteInquiry);
+inquiriesRouter.delete("/:id", requireToken, deleteInquiry);
 
 export default inquiriesRouter;
