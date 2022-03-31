af86c78f2d285dea782b1c1433d816782cfcc684
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-03-31 09:07:55 +0900

    finish login

--- frontend/pages/admin/login.tsx
+++ frontend/pages/admin/login.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
 import { useRouter } from "next/router";
-import { FormEventHandler, useRef } from "react";
+import { FormEventHandler, useContext, useRef } from "react";
 import Card from "../../components/UI/Card";
 import FormInput from "../../components/UI/input/FormInput";
 import FormSubmit from "../../components/UI/input/FormSubmit";
+import { AuthContext } from "../../context/auth-context";
 
 const Login: NextPage = () => {
   const router = useRouter();
+  const context = useContext(AuthContext);
 
   const usernameRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
@@ -15,6 +17,29 @@ const Login: NextPage = () => {
     event.preventDefault();
     const username = usernameRef.current!.value.trim();
     const password = passwordRef.current!.value.trim();
+
+    if (username && password) {
+      const data = { username, password };
+      fetch(`${process.env.NEXT_PUBLIC_API_URI}/users/login`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "POST",
+        body: JSON.stringify(data),
+        credentials: "include",
+      })
+        .then(result => {
+          if (!result.ok)
+            throw Error(`Login failed: ${result.status} ${result.statusText}`);
+          return result.json();
+        })
+        .then(data => {
+          if (data.authenticated) {
+            context.setIsAuth(true);
+            router.replace("/admin");
+          }
+        });
+    }
   };
 
   return (
