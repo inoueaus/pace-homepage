76953f6d76d855dc1479354329ed13dfd61bc99d
Austin Mayer <a.inoue@churadata.okinawa>
2022-03-31 17:55:42 +0900

    add body parser and settings for payload size

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,18 +6,26 @@ import postsRouter from "./posts";
 import usersRouter from "./users";
 import inquiriesRouter from "./inquiries";
 import { envVars } from "./env-variables";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
 app.use(cookieParser(envVars.tokenKey));
 app.use(
   cors({
     origin: envVars.nextServerUrl,
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
     credentials: true,
+  })
+);
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "50mb",
+    extended: true,
+    parameterLimit: 50000,
   })
 );
 
