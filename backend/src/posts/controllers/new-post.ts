e0ef8addcca380a5c1c605f877917ddf487b3adf
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-04-01 08:13:14 +0900

    fix new post image

--- backend/src/posts/controllers/new-post.ts
+++ backend/src/posts/controllers/new-post.ts
@@ -10,19 +10,30 @@ const newPost = async (req: Request, res: Response) => {
   try {
     const picture =
       typeof pictureProvided === "string"
-        ? convertB64StringToHex(pictureProvided) // must do some security work here
+        ? Buffer.from(pictureProvided, "base64url") // must do some security work here
         : null; // convert base64 image to hex binary
     if (typeof title !== "string") throw Error("No title provided.");
     if (typeof body !== "string") throw Error("No body provided");
 
+    console.log(picture, picture?.toString("base64"));
     const [result] =
       await sql`INSERT INTO posts (title, body, created_at, updated_at)
     VALUES (${title}, ${body}, NOW(), NOW())
     RETURNING id;`;
 
+    if (typeof result.id !== "number")
+      throw Error("Id not returned from DB on insert");
 
-    res.status(201).json({ id: result.id, created: true });
+    if (picture) {
+      const [imgResult] = await sql`INSERT INTO images (img, img_name, post_id)
+      VALUES (${picture}, ${String(result.id) + String(new Date().getTime())}, ${
+        result.id
+      }) RETURNING img_id AS id`;
+    }
+
+    res.status(201).json({ id: result.id, created: true, });
   } catch (error) {
+    console.log(error);
     res.statusCode = 400;
     if (error instanceof Error) res.json({ message: error.message });
   }
