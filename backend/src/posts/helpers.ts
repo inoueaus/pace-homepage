21e923bdb91f892ae46ffbb998438450e073adc5
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-04-01 09:32:17 +0900

    delete hex convert

--- backend/src/posts/helpers.ts
+++ backend/src/posts/helpers.ts
@@ -1,9 +1,6 @@
-export const convertB64StringToHex = (b64String: string) =>
-  Buffer.from(b64String, "base64").toString("hex");
-
 export const processProvidedImage = (base64String: string | undefined) => {
   if (typeof base64String === "string" && !base64String.includes(";")) {
-    return Buffer.from(base64String, "base64") // must do some security work here
+    return Buffer.from(base64String, "base64"); // must do some security work here
   }
-  return null
+  return null;
 };
