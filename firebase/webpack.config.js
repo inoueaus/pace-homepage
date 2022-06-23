9dec4c43923e2c0cf1b84f6fcf6339ddd03c53b7
Austin Mayer <a.inoue@churadata.okinawa>
2022-06-23 15:22:47 +0900

    setup firebase packages and webpack

---
+++ firebase/webpack.config.js
@@ -0,0 +1,83 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const fs = require("fs");
+const webpack = require("webpack");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+
+const port = 9001;
+
+module.exports = {
+  entry: { main: "./src/index.ts" },
+  mode: "development",
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@web-components": path.resolve(__dirname, "src/web-components/"),
+      "@firebase-logic": path.resolve(__dirname, "src/firebase.ts"),
+    },
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "public"),
+    },
+    https: {
+      key: fs.readFileSync("./key.pem"),
+      cert: fs.readFileSync("./cert.pem"),
+    },
+    compress: true,
+    port,
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  plugins: [
+    new WebpackManifestPlugin({ fileName: "public-manifest.json" }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+      chunks: ["main"],
+    }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "development",
+      FRONTEND_URI: `https://localhost:${port}/`,
+      NOTIFICATION_URI: "https://shopping-list-notifications.herokuapp.com/",
+    }),
+  ],
+};
---
+++ firebase/webpack.prod.config.js
@@ -0,0 +1,86 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
+const { WebpackManifestPlugin } = require("webpack-manifest-plugin");
+
+module.exports = {
+  entry: "./src/index.ts",
+  mode: "production",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+          "ts-loader",
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@web-components": path.resolve(__dirname, "src/web-components/"),
+      "@firebase-logic": path.resolve(__dirname, "src/firebase.ts"),
+    },
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "[name].[fullhash].js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+    new WebpackManifestPlugin({ fileName: "public-manifest.json", filter: (file) => file.name.includes(".js") }),
+    new MiniCssExtractPlugin({ filename: (data) => `${data.chunk.name}.${data.hash}.css` }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public", filter: (path) => !path.includes(".html") }],
+    }),
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: "production",
+      FRONTEND_URI: "https://shopping-list-app-d0386.web.app/",
+    }),
+  ],
+};
