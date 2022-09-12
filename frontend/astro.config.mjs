19943e8cb44d2b9ae1950d620ee6cf70306cd9ff
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-09-12 14:18:00 +0900

    update settings

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 import lit from "@astrojs/lit";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), lit()]
+  integrations: [react(), lit()],
+  publicDir: "./public",
+  output: "static", // this tells astro that the site will be built to static hosting, not a running server
 });
