807c65bdd1f811d79ac65bc2588c6ca2f3245cc6
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-09-12 09:53:11 +0900

    get image cards working

--- frontend/src/components/UI/ImageCard.jsx
+++ frontend/src/components/UI/ImageCard.jsx
@@ -1,11 +1,10 @@
 import styles from "./ImageCard.module.css";
 
 const ImageCard = props => {
-  console.log(`${styles["image-section"]} ${styles[props.className]}`);
   return (
     <div className={`${styles.card} ${styles[props.className]}`}>
       <section className={styles["image-section"]}>
-        <img src={props.image} />
+        <img className={styles["image"]} src={props.image} />
       </section>
       <section className={styles["text-section"]}>{props.children}</section>
     </div>
