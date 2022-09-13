bc3f9811fff062144e3b56b6437bee67e092b367
Austin Mayer <95405354+tronicboy1@users.noreply.github.com>
2022-09-13 10:01:44 +0900

    get blog post loading spinner working

--- frontend/src/components/blog/BlogList.tsx
+++ frontend/src/components/blog/BlogList.tsx
@@ -2,8 +2,8 @@ import Observer from "../../components/UI/Observer";
 import { useEffect, useState } from "react";
 import BlogPost from "../../components/blog/BlogPost";
 import UnorderedList from "../../components/UnorderedList";
-import LoadingSpinner from "../UI/LoadingSpinner";
 import type { PostModel } from "../../../types/post-model";
+import styles from "./BlogPost.module.css";
 
 const BlogList: React.FC<{ path: string }> = ({ path }) => {
   const [page, setPage] = useState(0);
@@ -44,8 +44,18 @@ const BlogList: React.FC<{ path: string }> = ({ path }) => {
   }, [inView]);
 
   return (
-    <div>
-      {loading && !allLoaded && <LoadingSpinner></LoadingSpinner>}
+    <>
+      {loading && posts.length === 0 && <div className={styles["spinner-container"]}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="#de9e48"
+          className={styles.spinner}
+        >
+          <path d="M0 0h24v24H0V0z" fill="none" />
+          <path d="M12 6v3l4-4-4-4v3c-4.42 0-8 3.58-8 8 0 1.57.46 3.03 1.24 4.26L6.7 14.8c-.45-.83-.7-1.79-.7-2.8 0-3.31 2.69-6 6-6zm6.76 1.74L17.3 9.2c.44.84.7 1.79.7 2.8 0 3.31-2.69 6-6 6v-3l-4 4 4 4v-3c4.42 0 8-3.58 8-8 0-1.57-.46-3.03-1.24-4.26z" />
+        </svg>
+      </div>}
       <UnorderedList>
         {posts &&
           posts.map(post => (
@@ -55,7 +65,7 @@ const BlogList: React.FC<{ path: string }> = ({ path }) => {
           ))}
       </UnorderedList>
       <Observer setInView={setInview} />
-    </div>
+    </>
   );
 };
 
