dffcdce553a31a2f1ce16bf0134dcf2eb0ec5822
inoueaus <87982396+inoueaus@users.noreply.github.com>
2022-09-25 08:59:19 +0900

    add docker compose

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,9 @@ export default defineConfig({
   integrations: [react(), lit()],
   publicDir: "./public",
   output: "static", // this tells astro that the site will be built to static hosting, not a running server
+  site: "https://www.pace-coffee.com",
+  server: {
+    host: "0.0.0.0",
+    port: 3000,
+  },
 });
